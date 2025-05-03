b764089c4656addb6ddf022fc2163f7215f749c5
ntutlaygmailcom <ntutlay@gmail.com>
2025-05-03 10:53:37 +0800

    0503

--- JS/hrfrontbtngroup.js
+++ JS/hrfrontbtngroup.js
@@ -1,10 +1,20 @@
 const btnGroup = document.querySelectorAll(".mockup .btn-group .btn");
 console.log(btnGroup);
+const situationgroup = document.querySelectorAll(".situation");
+console.log(situationgroup);
 btnGroup.forEach((btn) => {
   btn.addEventListener("click", () => {
     btnGroup.forEach((b) => {
       b.classList.remove("first");
     });
+    const name = btn.dataset.name;
+    situationgroup.forEach((situation) => {
+      situation.classList.remove("active");
+      if (situation.dataset.name === name) {
+        situation.classList.add("active");
+      }
+    });
+    console.log(btn.dataset.name);
     btn.classList.add("first");
   });
 });
